947e72185a7fc199d323e614e231dc56b6c0e2e8
chanddavid <tankamansodari7@gmail.com>
2022-03-30 16:31:22 +0545

    day 1 in Python Udemy course

---
+++ Day1/dictionary.py
@@ -0,0 +1,12 @@
+car =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+# keys=dict.fromkeys(car)
+# keys=car.get("brand")
+# keys=car.keys
+# value=car.values
+popp=car.pop("year")
+updat=car.update({"brand":"akon"})
+print(car)
---
+++ Day1/exercise.py
@@ -0,0 +1,14 @@
+import re
+
+
+d='django'
+# print(d[0])
+# print(d[-1])
+# print(d[0:4])
+# print(d[1:4])
+# print(d[4:6])
+
+
+reassign=[3,5,[1,4,'hello']]
+reassign[2][2]='goodbuy'
+print(reassign)
